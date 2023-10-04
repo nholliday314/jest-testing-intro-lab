1485d0f26c728ba3f528c686c62be79832e76d7a
nholliday314 <nholliday314@gmail.com>
2023-10-04 15:30:23 -0700

    finished base and edge cases

--- isSameFirstLastCharacter.test.js
+++ isSameFirstLastCharacter.test.js
@@ -12,17 +12,27 @@ test("it should return true when the first and last character are the same", () 
 /* A test case for a string that does NOT have the same first and last 
    character and is 4+ characters long
 */
-test("", () => {});
+test("it should return false when the first and last character are NOT the same", () => {
+   expect(isSameFirstLastCharacter("abbbbb")).toBe(false);
+});
 
 // Edge Cases
 // A test case for a string with only 2 characters that are different
-test("", () => {});
+test("it should return false when the 2 characters are NOT the same", () => {
+   expect(isSameFirstLastCharacter("ab")).toBe(false);
+});
 
 // A test case for a string with only 2 characters that are the same
-test("", () => {});
+test("it should return true when the 2 characters are  the same", () => {
+   expect(isSameFirstLastCharacter("aa")).toBe(true);
+});
 
 // A test case for a string with only 1 character
-test("", () => {});
+test("it should return true when only 1 character", () => {
+   expect(isSameFirstLastCharacter("a")).toBe(true);
+});
 
 // A test case for an empty string
-test("", () => {});
+test("it should return true", () => {
+   expect(isSameFirstLastCharacter("")).toBe(true);
+});
